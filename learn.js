3f5c454597eba3e26c46401601e6479a6f87ef94
yaserJaja <yaser.jaja.programmer@hotmail.com>
2022-08-12 05:56:03 +0300

    git update final course files

--- learn.js
+++ learn.js
@@ -1,3 +1,7 @@
+// Resource
+// https://elzero.org/sass-2021-the-end-and-how-to-master-sass/
+
+
 /// Lessons:
 
 // [1] import
@@ -8,5 +12,7 @@
 // [6] Placeholder
 // [7] condition
 // [8] comment
-// [9] mixin
+// [9] mixin with content
 // [10] interpolation 
+// [11] Loops (for, each, while)
+// [12] function
